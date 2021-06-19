8b29de657b749c2dd87363c9aed21ac93a69d838
Damian Galli <damian.galli@galli.site>
2021-06-19 22:55:57 +0200

    Refcator Templates add Preprocess Functions

---
+++ src/PreprocessTwig.php
@@ -0,0 +1,28 @@
+<?php
+namespace App;
+
+class PreprocessTwig {
+
+    public static function preprocessTemplateVariables($variables,$template) {
+        if(!is_array($variables)) {
+            $variables = [];
+        }
+        //get template if it was not overwritten on item level
+        if(!array_key_exists('template',$variables)) {
+            $variables['template'] = ConfigManager::getInstance()->getOverwrittenGlobalTemplates($template,$variables);
+        }
+        $variables = HookManager::trigger("alter_template_data".str_replace(['/','.twig'],['_',''],$variables['template']),[
+            'variables' => $variables
+        ],'variables');
+        return $variables;
+    }
+
+    public static function preprocessCssClasses($defaultClasses,$template,$overwrittenClasses) {
+        if($overwrittenClasses !== NULL) {
+            return $overwrittenClasses;
+        }
+        //check for global overwrittes
+        return ConfigManager::getInstance()->getOverwrittenGlobalClasses($template,$defaultClasses);
+    }
+
+}
